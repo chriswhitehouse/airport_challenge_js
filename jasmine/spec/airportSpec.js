e52203f8c6ab2f9e80b7cb7bbf55dd4036141946
Natasha McIntyre <natashamc.cs@gmail.com>
2021-01-12 14:51:25 +0000

    take_off method test

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -1,7 +1,13 @@
 describe('Airport', function () {
   it('should land a plane', function() {
     airport = new Airport();
-    plane = new Plane();
-    expect(airport.land(plane)).toBe(plane)
+    // plane = new Plane();
+    expect(airport.land('plane')).toBe('plane')
+  });
+
+  it('should instruct a plane to depart', function() {
+    airport = new Airport();
+    airport.land('plane');
+    expect(airport.take_off('plane')).toBe('plane')
   });
 });
